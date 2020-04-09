900e4002257a26fa1f6c55692a4d8a3876e57a24
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-04-10 01:58:13 +0400

    homework

---
+++ FirstLesson/FirstLesson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FirstLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter your Name");
+            string Name = ReadLine();
+            WriteLine("Enter your age");
+            int age =int.Parse(ReadLine());
+            WriteLine("Enter your Location");
+            string Location = ReadLine();
+            WriteLine("Enter your Profession");
+            string Profession = ReadLine();
+            WriteLine("Enter your Goroskope");
+            char Goroskope = char.Parse(ReadLine());
+            StringBuilder sb = new StringBuilder($"WELCOME {Name}");
+            string str = sb.ToString();
+            ForegroundColor = ConsoleColor.Red;
+            SetCursorPosition(WindowWidth / 2, WindowHeight / 2);
+            WriteLine(str);
+
+        }
+    }
+}
